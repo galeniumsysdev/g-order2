7e1d7800ce02b11079e66d571e2e497f116c2fae
Sulis Triyono <sulis.tryono@gmail.com>
2017-10-11 18:00:56 +0700

    Update dpl/list table

--- resources/views/admin/dpl/dplList.blade.php
+++ resources/views/admin/dpl/dplList.blade.php
@@ -13,36 +13,45 @@
     </div>
   @endif
 
-  <table class="table table-striped table-hover">
-    <thead>
-      <tr>
-        <th>Suggest No.</th>
-        <th>Last Approver</th>
-        <th>DPL No.</th>
-        <th>MR</th>
-        <th>Outlet</th>
-        <th>Distributor</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($dpl as $list)
-      <tr>
-        <td align="center">{{ $list->suggest_no }}</td>
-        <td>{{ $list->dpl_appr_name }}</td>
-        <td align="center">{{ $list->dpl_no }}</td>
-        <td>{{ $list->dpl_mr_name }}</td>
-        <td>{{ $list->dpl_outlet_name }}</td>
-        <td>{{ $list->dpl_distributor_name }}</td>
-        <td align="center">
-          {!! $list->btn_discount !!}
-          {!! $list->btn_confirm !!}
-          {!! $list->btn_dpl_no !!}
-        </td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+  <div class="container dpl-container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="panel panel-default">
+          <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>Suggest No.</th>
+                <th>Last Approver</th>
+                <th>DPL No.</th>
+                <th>MR</th>
+                <th>Outlet</th>
+                <th>Distributor</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($dpl as $list)
+              <tr>
+                <td style="vertical-align: middle;">{{ $list->suggest_no }}</td>
+                <td style="vertical-align: middle;">{{ $list->dpl_appr_name }}</td>
+                <td style="vertical-align: middle;">{{ $list->dpl_no }}</td>
+                <td style="vertical-align: middle;">{{ $list->dpl_mr_name }}</td>
+                <td style="vertical-align: middle;" width="200">{{ $list->dpl_outlet_name }}</td>
+                <td style="vertical-align: middle;">{{ $list->dpl_distributor_name }}</td>
+                <td style="vertical-align: middle;">
+                  {!! $list->btn_discount !!}
+                  {!! $list->btn_confirm !!}
+                  {!! $list->btn_dpl_no !!}
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
 @section('js')
 
