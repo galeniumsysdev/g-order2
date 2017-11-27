11c5b3b4c2c245ba0795380d77da5bcb9a119629
Willi Kristianto <willi.kristianto@wk-productions.com>
2017-11-27 10:11:48 +0700

    dpl no notification and insert to table so_headers

--- resources/views/admin/dpl/dplNoForm.blade.php
+++ resources/views/admin/dpl/dplNoForm.blade.php
@@ -18,7 +18,7 @@
     <div class="row">
       <div class="col-md-10 col-sm-offset-1">
         <div class="panel panel-default">
-          <div class="panel-heading"><strong>Discount Form</strong></div>
+          <div class="panel-heading"><strong>DPL No. Form</strong></div>
           <div class="panel-body" style="overflow-x:auto;">
             <div class="panel panel-default">
               <div class="form-wrapper">
