f250d661279a48538ea90c259a47cee4f69bffb7
Solinda <shanty.dewi@solinda.co.id>
2018-02-06 13:56:50 +0700

    report order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -668,7 +668,7 @@ class OrderController extends Controller
           or ($afterheader->qty_confirm_primary==0 and $afterheader->qty_shipping_primary==$afterheader->qty_request_primary)){
               $header->status = 3;
           }else{ $header->status = 2;}
-          $header->save();          
+          $header->save();
           $dist=Customer::where('id','=',$header->distributor_id)->first();
           $content = 'Pesanan Anda dengan PO nomor: <strong>'.$header->customer_po.'</strong> dan SJ: '.$request->deliveryno.' telah diterima customer pada tanggal: '.Carbon::now().'.<br>';
           $content .='Terimakasih telah menggunakan aplikasi '.config('app.name', 'g-Order');
@@ -941,6 +941,7 @@ class OrderController extends Controller
         return view('admin.report.order',compact('provinces','request','channels'));
       }else{
         //echo ("tglaw:".$request->tglaw."tgl_akhir:".$request->tglak);
+        DB::enableQueryLog();
         $namapropinsi=null;
         $nmchannel=null;
         $datalist = DB::table('so_header_v as sh')
@@ -1009,7 +1010,7 @@ class OrderController extends Controller
           $datalist =$datalist->groupBy('notrx', 'customer_name', 'tgl_order', 'distributor_name', 'alamat', 'status_name', 'divisi', 'channel', 'tgl_approve'
 	                                     , 'id', 'line_id', 'product_id', 'title', 'amount', 'qty_request_primary', 'unit_price_primary', 'deliveryno', 'tgl_terima', 'tgl_kirim');
           $datalist =$datalist->get();
-
+          //dd(DB::getQueryLog() );
         Excel::create('Order-'.Carbon::now(), function($excel) use($datalist,$request,$namapropinsi,$nmchannel) {
             $excel->sheet('order', function($sheet) use($datalist,$request,$namapropinsi,$nmchannel) {
                 $sheet->loadView('admin.report.orderview',array('datalist'=>$datalist,'request'=>$request,'namapropinsi'=>$namapropinsi,'nmchannel'=>$nmchannel));
